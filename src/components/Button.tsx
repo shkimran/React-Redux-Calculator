043f4a4f487a2df714e371d9efe16d1f08cc3c35
shkimran <imranshk.ek@gmail.com>
2025-06-11 20:02:53 +0530

    style: enhance button styles with shadows and improved hover effects

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,10 +17,10 @@ const Button: React.FC<ButtonProps> = React.memo(({ value }) => {
     <button
       className={`
         ${isZero ? 'col-span-2 text-left pl-6' : ''}
-        ${isOp ? 'bg-orange-500 text-white' : ''}
-        ${isUtil ? 'bg-gray-300 text-black' : ''}
-        ${!isOp && !isUtil ? 'bg-neutral-700 text-white' : ''}
-        text-xl font-medium py-4 rounded-full hover:brightness-110 transition-all
+        ${isOp ? 'bg-orange-500 text-white shadow-[inset_0px_3px_3px_#ffffff]' : ''}
+        ${isUtil ? 'bg-gray-400 text-black shadow-[inset_0px_4px_4px_#ffffff]' : ''}
+        ${!isOp && !isUtil ? 'bg-neutral-700 text-white shadow-[inset_0px_3px_2px_#ffffff91]' : ''}
+        text-xl py-4 rounded-full hover:brightness-125 transition-all
       `}
       onClick={() => dispatch(handleButton(value))}
     >
